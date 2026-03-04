5c517643c6256ee35a3bf50a1cf9dc70a5d28879
Sieg Collado <siegcollado@gmail.com>
2026-03-05 07:20:40 +0800

    feat: add keymaps to tmux navigation

--- config/keymaps/navigation.nix
+++ config/keymaps/navigation.nix
@@ -1,48 +1,48 @@
 {
   keymaps = [
     # Window navigation
-    {
-      mode = "n";
-      key = "<C-h>";
-      action = "<C-w>h";
-      options = {
-        desc = "Go to Left Window";
-        remap = true;
-      };
-    }
-    {
-      mode = "n";
-      key = "<C-j>";
-      action = "<C-w>j";
-      options = {
-        desc = "Go to Lower Window";
-        remap = true;
-      };
-    }
-    {
-      mode = "n";
-      key = "<C-k>";
-      action = "<C-w>k";
-      options = {
-        desc = "Go to Upper Window";
-        remap = true;
-      };
-    }
-    {
-      mode = "n";
-      key = "<C-l>";
-      action = "<C-w>l";
-      options = {
-        desc = "Go to Right Window";
-        remap = true;
-      };
-    }
-    {
-      mode = "n";
-      key = "<leader>w/";
-      action = "<C-w>v";
-      options.desc = "Split Window Right";
-    }
+    # {
+    #   mode = "n";
+    #   key = "<C-h>";
+    #   action = "<C-w>h";
+    #   options = {
+    #     desc = "Go to Left Window";
+    #     remap = true;
+    #   };
+    # }
+    # {
+    #   mode = "n";
+    #   key = "<C-j>";
+    #   action = "<C-w>j";
+    #   options = {
+    #     desc = "Go to Lower Window";
+    #     remap = true;
+    #   };
+    # }
+    # {
+    #   mode = "n";
+    #   key = "<C-k>";
+    #   action = "<C-w>k";
+    #   options = {
+    #     desc = "Go to Upper Window";
+    #     remap = true;
+    #   };
+    # }
+    # {
+    #   mode = "n";
+    #   key = "<C-l>";
+    #   action = "<C-w>l";
+    #   options = {
+    #     desc = "Go to Right Window";
+    #     remap = true;
+    #   };
+    # }
+    # {
+    #   mode = "n";
+    #   key = "<leader>w/";
+    #   action = "<C-w>v";
+    #   options.desc = "Split Window Right";
+    # }
 
     # Window resize
     {
--- config/plugins/nvim-tmux-navigation.nix
+++ config/plugins/nvim-tmux-navigation.nix
@@ -1,9 +1,58 @@
-{ lib, ... }:
 {
   plugins.tmux-navigator = {
     enable = true;
+
     settings = {
+      no_mappings = 1;
       disable_when_zoomed = 1;
+
     };
+
+    keymaps =
+      let
+        mode = [
+          "n"
+          "x"
+        ];
+        keys = [
+          {
+            key = "h";
+            action = "left";
+          }
+          {
+            key = "j";
+            action = "down";
+          }
+          {
+            key = "k";
+            action = "up";
+          }
+          {
+            key = "l";
+            action = "right";
+          }
+          {
+            key = "\\";
+            action = "previous";
+          }
+        ];
+      in
+      builtins.concatMap (keymap: [
+        {
+          inherit mode;
+          key = "<C-w>" + keymap.key;
+          inherit (keymap) action;
+        }
+        {
+          inherit mode;
+          key = "<C-${keymap.key}>";
+          inherit (keymap) action;
+        }
+        {
+          inherit mode;
+          key = "<C-w><C-${keymap.key}>";
+          inherit (keymap) action;
+        }
+      ]) keys;
   };
 }
