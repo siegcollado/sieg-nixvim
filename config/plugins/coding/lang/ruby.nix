8283adbf8f2010ec247751a2d01591b401f98ebd
Sieg Collado <siegcollado@gmail.com>
2026-03-06 02:17:29 +0800

    fix: require projects to install ruby-lsp in their environment

--- config/plugins/coding/lang/ruby.nix
+++ config/plugins/coding/lang/ruby.nix
@@ -3,8 +3,21 @@
     conform-nvim.settings.formatters_by_ft.ruby = [ "rubocop" ];
 
     lsp.servers = {
-      ruby_lsp.enable = true;
-      rubocop.enable = true; # not sure...
+      ruby_lsp = {
+        enable = true;
+        package = null; # use the ruby-lsp in the project.
+        # extraOptions = {
+        # init_options = {
+        #   enabledFeatures = {
+        #     diagnostics = false;
+        #     # codeActions = true;
+        #   };
+        # };
+        # };
+      };
+      rubocop = {
+        enable = true;
+      };
     };
 
     neotest.adapters.rspec = {
