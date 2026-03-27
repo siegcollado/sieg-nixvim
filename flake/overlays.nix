c662ef219b6c0c47ed7f6ceeabc2ec0b0751b7ea
Sieg Collado <siegcollado@gmail.com>
2026-03-27 12:40:10 +0800

    chore: remove ast-grep overlay for grug-far

--- flake/overlays.nix
+++ flake/overlays.nix
@@ -1,23 +1,6 @@
 { inputs, ... }:
 {
   flake.overlays.default = _: prev: {
-    # NOTE: Work around nixpkgs ast-grep regression that breaks grug-far.
-    # Remove once this lands https://github.com/NixOS/nixpkgs/pull/498607
-    ast-grep = prev.ast-grep.overrideAttrs (_: rec {
-      version = "0.40.5";
-      src = prev.fetchFromGitHub {
-        owner = "ast-grep";
-        repo = "ast-grep";
-        tag = version;
-        hash = "sha256-O4f9PjGtwK6poFIbtz26q8q4fiYjfQEtobXmghQZAfw=";
-      };
-
-      cargoHash = "sha256-N5WrItW/yeZ+GDTw5yFy4eB11BzOlcuePGAefhJaG6I=";
-      cargoDeps = prev.rustPlatform.fetchCargoVendor {
-        inherit src;
-        hash = cargoHash;
-      };
-    });
     vimPlugins = prev.vimPlugins // {
       exrc-nvim = prev.vimUtils.buildVimPlugin {
         pname = "exrc.nvim";
